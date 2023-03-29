5150ccdf5bb66af79f601730d9a5d851f6d6ae4a
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2023-03-29 20:49:14 -0300

    Update ufrpeChat.py

--- ufrpeChat.py
+++ ufrpeChat.py
@@ -1,13 +1,15 @@
+import csv
 import openai
+import re
+
 
 # Substitua "sk-..." pela sua chave de API secreta
-openai.api_key = ""
+openai.api_key = "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"
 
 # Escolha um modelo da open ai (por exemplo, davinci)
 model = "text-davinci-003"
 
 # Leia o arquivo CSV usando o módulo csv do python
-import csv
 data = []
 with open("Data.csv", encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile)
@@ -15,6 +17,8 @@ with open("Data.csv", encoding="utf-8") as csvfile:
         data.append(row[0])
 
 # Crie uma função que recebe uma pergunta do usuário e gera um texto usando o modelo da open ai
+
+
 def generate_text(question):
     # Construa a entrada para a API da open ai com os dados do CSV e a pergunta do usuário
     input = "Dados:\n"
@@ -27,7 +31,7 @@ def generate_text(question):
         engine=model,
         prompt=input,
         temperature=0.5,
-        max_tokens=100,
+        max_tokens=300,
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.0,
@@ -37,11 +41,20 @@ def generate_text(question):
     # Retorne a resposta gerada pelo modelo
     return response["choices"][0]["text"]
 
+
 # Teste a função com uma pergunta de exemplo
 while True:
 
     question = input("Digite uma pergunta: ")
+    #words = question.lower().split()
     if question == "quit":
-        break
+        break    
+    #if re.search(r'\bbolsa(s)?\b', question.lower()) or re.search(r'iniciação cientifica', question.lower()):
+      #with open("Bolsas.csv", encoding="utf-8") as csvfile:
+        #reader = csv.reader(csvfile)
+        #for row in reader:
+            #data.append(row[0])
+      
     answer = generate_text(question)
+  
     print(answer)
